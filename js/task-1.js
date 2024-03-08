f200ff15eca6c872549c268a2223febf94aed231
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-03-08 15:40:20 +0200

    task-1 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,18 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const categoriesListEl = document.querySelector('#categories');
+const categoryEl = document.querySelectorAll('.item');
+const categoriesTitlesEl = document.querySelectorAll('.item h2');
+const categoryListEl = document.querySelectorAll('.item ul');
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+function calcCategories(element) {
+  return `Number of categories: ${element.children.length}`;
+}
+console.log(calcCategories(categoriesListEl));
+
+function getCategoryInfo(arr) {
+  return arr.forEach((element, index) => {
+    console.log(`Category: ${element.textContent}`);
+    console.log(`Elements: ${categoryListEl[index].children.length}`);
+  });
+}
+
+getCategoryInfo(categoriesTitlesEl);
