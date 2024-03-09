e28d44f06aec69337d6057be2c1a08590809941c
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-03-09 12:24:40 +0200

    task-1 updated (easier solution)

--- js/task-1.js
+++ js/task-1.js
@@ -3,16 +3,22 @@ const categoryEl = document.querySelectorAll('.item');
 const categoriesTitlesEl = document.querySelectorAll('.item h2');
 const categoryListEl = document.querySelectorAll('.item ul');
 
-function calcCategories(element) {
-  return `Number of categories: ${element.children.length}`;
-}
-console.log(calcCategories(categoriesListEl));
+// function calcCategories(element) {
+//   return `Number of categories: ${element.children.length}`;
+// }
+// console.log(calcCategories(categoriesListEl));
 
-function getCategoryInfo(arr) {
-  return arr.forEach((element, index) => {
-    console.log(`Category: ${element.textContent}`);
-    console.log(`Elements: ${categoryListEl[index].children.length}`);
-  });
-}
+// function getCategoryInfo(arr) {
+//   return arr.forEach((element, index) => {
+//     console.log(`Category: ${element.textContent}`);
+//     console.log(`Elements: ${categoryListEl[index].children.length}`);
+//   });
+// }
 
-getCategoryInfo(categoriesTitlesEl);
+// getCategoryInfo(categoriesTitlesEl);
+
+console.log(categoryEl.length);
+categoryEl.forEach(element => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
