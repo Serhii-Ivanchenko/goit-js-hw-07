84223a04e294bb780596bd016fa615b204d63118
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-03-09 16:04:45 +0200

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector('.change-color');
+const colorNameEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = `${getRandomHexColor()}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
